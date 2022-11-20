a90bf07d4c77c25748a558d1e05ffeb0dbe60c3d
wcs <wcs78929@gmail.com>
2022-11-20 17:22:28 +0800

    fix: 修复报错 node 19
    Signed-off-by: wcs <wcs78929@gmail.com>

--- 3. function template/template.cpp
+++ 3. function template/template.cpp
@@ -1,34 +1,39 @@
+#include "v8-context.h"
 #include <node.h>
 
 namespace __template__ {
 
-using v8::HandleScope;
-using v8::FunctionTemplate;
-using v8::FunctionCallbackInfo;
 using v8::Function;
+using v8::FunctionCallbackInfo;
+using v8::FunctionTemplate;
+using v8::HandleScope;
 using v8::Isolate;
 using v8::Local;
 using v8::Object;
 using v8::String;
 using v8::Value;
 
-void Method(const FunctionCallbackInfo<Value>& args)
-{
-    Isolate* isolate = args.GetIsolate();
-    args.GetReturnValue().Set(String::NewFromUtf8(isolate, "this is a function"));
+void Method(const FunctionCallbackInfo<Value> &args) {
+  Isolate *isolate = args.GetIsolate();
+  args.GetReturnValue().Set(
+      String::NewFromUtf8(isolate, "this is a function").ToLocalChecked());
 }
 
-void init(Local<Object> exports)
-{
-    Isolate* isolate = Isolate::GetCurrent();
-    HandleScope scope(isolate);
-    Local<FunctionTemplate> t = FunctionTemplate::New(isolate, Method);
-    Local<Function> fn = t->GetFunction();
-    Local<String> name = String::NewFromUtf8(isolate, "funcCreateByTemplate");
-    fn->SetName(name);
-    exports->Set(name, fn);
+void init(Local<Object> exports) {
+  Isolate *isolate = Isolate::GetCurrent();
+  HandleScope scope(isolate);
+  Local<FunctionTemplate> t = FunctionTemplate::New(isolate, Method);
+  Local<Function> fn =
+      t->GetFunction(v8::Context::New(isolate)).ToLocalChecked();
+  Local<String> name =
+      String::NewFromUtf8(isolate, "funcCreateByTemplate").ToLocalChecked();
+  fn->SetName(name);
+  // 警告 Ignoring return value of function declared with 'warn_unused_result'
+  // attribute。转为 void
+  // https://stackoverflow.com/questions/43238446/ignoring-return-value-of-function-declared-with-warn-unused-result-attribute
+  (void)exports->Set(v8::Context::New(isolate), name, fn);
 }
 
 NODE_MODULE(_template, init)
 
-}
+} // namespace __template__
