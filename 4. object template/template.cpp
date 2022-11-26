64793cd72ad7ef7c5415252e516977978c6ffdb7
wcs <wcs78929@gmail.com>
2022-11-26 11:42:57 +0800

    fix: 4. object template 报错
    Signed-off-by: wcs <wcs78929@gmail.com>

--- 4. object template/template.cpp
+++ 4. object template/template.cpp
@@ -1,73 +1,89 @@
+#include "v8-context.h"
 #include <node.h>
 
 namespace __template__ {
 
+using v8::Array;
 using v8::FunctionCallbackInfo;
-using v8::HandleScope;
-using v8::ObjectTemplate;
 using v8::FunctionTemplate;
-using v8::Function;
+using v8::HandleScope;
 using v8::Isolate;
 using v8::Local;
+using v8::Number;
 using v8::Object;
+using v8::ObjectTemplate;
 using v8::String;
 using v8::Value;
-using v8::Number;
-using v8::Array;
 
-void Constructor(const FunctionCallbackInfo<Value>& args)
-{
-    Isolate* isolate = args.GetIsolate();
-    args.This()->Set(String::NewFromUtf8(isolate, "value"), Number::New(isolate, 233));
-    return args.GetReturnValue().Set(args.This());
+void Constructor(const FunctionCallbackInfo<Value> &args) {
+  Isolate *isolate = args.GetIsolate();
+  (void)args.This()->Set(v8::Context::New(isolate),
+                         String::NewFromUtf8(isolate, "value").ToLocalChecked(),
+                         Number::New(isolate, 233));
+  return args.GetReturnValue().Set(args.This());
 }
 
-void ClassGet(const FunctionCallbackInfo<Value>& args)
-{
-    Isolate* isolate = args.GetIsolate();
-    return args.GetReturnValue().Set(args.This()->Get(String::NewFromUtf8(isolate, "value")));
+void ClassGet(const FunctionCallbackInfo<Value> &args) {
+  Isolate *isolate = args.GetIsolate();
+  return args.GetReturnValue().Set(
+      args.This()
+          ->Get(v8::Context::New(isolate),
+                String::NewFromUtf8(isolate, "value").ToLocalChecked())
+          .ToLocalChecked());
 }
 
-void Func(const FunctionCallbackInfo<Value>& args)
-{
-    args.GetReturnValue().Set(2333);
+void Func(const FunctionCallbackInfo<Value> &args) {
+  args.GetReturnValue().Set(2333);
 }
 
-void Init(Local<Object> exports)
-{
-    Isolate* isolate = Isolate::GetCurrent();
-    HandleScope scope(isolate);
+void Init(Local<Object> exports) {
+  Isolate *isolate = Isolate::GetCurrent();
+  HandleScope scope(isolate);
 
-    // 作为函数模板的原型
-    Local<FunctionTemplate> tpl = FunctionTemplate::New(isolate, Constructor);
-    tpl->SetClassName(String::NewFromUtf8(isolate, "TestClass"));
-    Local<ObjectTemplate> proto = tpl->PrototypeTemplate();
-    proto->Set(String::NewFromUtf8(isolate, "get"), FunctionTemplate::New(isolate, ClassGet));
+  // 作为函数模板的原型
+  Local<FunctionTemplate> tpl = FunctionTemplate::New(isolate, Constructor);
+  tpl->SetClassName(String::NewFromUtf8(isolate, "TestClass").ToLocalChecked());
+  Local<ObjectTemplate> proto = tpl->PrototypeTemplate();
+  proto->Set(String::NewFromUtf8(isolate, "get").ToLocalChecked(),
+             FunctionTemplate::New(isolate, ClassGet));
 
-    exports->Set(String::NewFromUtf8(isolate, "TestClass"), tpl->GetFunction());
+  (void)exports->Set(
+      v8::Context::New(isolate),
+      String::NewFromUtf8(isolate, "TestClass").ToLocalChecked(),
+      tpl->GetFunction(v8::Context::New(isolate)).ToLocalChecked());
 
-    // 作为对象模板创建对象
-    Local<FunctionTemplate> fun = FunctionTemplate::New(isolate);
-    fun->SetClassName(String::NewFromUtf8(isolate, "TestClass2"));
-    Local<ObjectTemplate> obj_tpl = ObjectTemplate::New(isolate, fun);
-    obj_tpl->Set(String::NewFromUtf8(isolate, "num"), Number::New(isolate, 233));
-    Local<Array> array = Array::New(isolate, 10);
-    for(int i = 0; i < 10; i++)
-    {
-        array->Set(Number::New(isolate, i), obj_tpl->NewInstance());
-    }
-    exports->Set(String::NewFromUtf8(isolate, "array"), array);
+  // 作为对象模板创建对象
+  Local<FunctionTemplate> fun = FunctionTemplate::New(isolate);
+  fun->SetClassName(
+      String::NewFromUtf8(isolate, "TestClass2").ToLocalChecked());
+  Local<ObjectTemplate> obj_tpl = ObjectTemplate::New(isolate, fun);
+  obj_tpl->Set(String::NewFromUtf8(isolate, "num").ToLocalChecked(),
+               Number::New(isolate, 233));
+  Local<Array> array = Array::New(isolate, 10);
+  for (int i = 0; i < 10; i++) {
+    (void)array->Set(
+        v8::Context::New(isolate), Number::New(isolate, i),
+        obj_tpl->NewInstance(v8::Context::New(isolate)).ToLocalChecked());
+  }
+  (void)exports->Set(v8::Context::New(isolate),
+                     String::NewFromUtf8(isolate, "array").ToLocalChecked(),
+                     array);
 
-    // 设置函数体
-    Local<ObjectTemplate> obj_with_func_tpl = ObjectTemplate::New(isolate);
-    obj_with_func_tpl->Set(String::NewFromUtf8(isolate, "cat"),
-            String::NewFromUtf8(isolate, "南瓜饼"));
-    obj_with_func_tpl->Set(String::NewFromUtf8(isolate, "dog"),
-            String::NewFromUtf8(isolate, "蛋花汤"));
-    obj_with_func_tpl->SetCallAsFunctionHandler(Func);
-    exports->Set(String::NewFromUtf8(isolate, "func"), obj_with_func_tpl->NewInstance());
+  // 设置函数体
+  Local<ObjectTemplate> obj_with_func_tpl = ObjectTemplate::New(isolate);
+  obj_with_func_tpl->Set(
+      String::NewFromUtf8(isolate, "cat").ToLocalChecked(),
+      String::NewFromUtf8(isolate, "南瓜饼").ToLocalChecked());
+  obj_with_func_tpl->Set(
+      String::NewFromUtf8(isolate, "dog").ToLocalChecked(),
+      String::NewFromUtf8(isolate, "蛋花汤").ToLocalChecked());
+  obj_with_func_tpl->SetCallAsFunctionHandler(Func);
+  (void)exports->Set(v8::Context::New(isolate),
+                     String::NewFromUtf8(isolate, "func").ToLocalChecked(),
+                     obj_with_func_tpl->NewInstance(v8::Context::New(isolate))
+                         .ToLocalChecked());
 }
 
 NODE_MODULE(_template, Init)
 
-}
+} // namespace __template__
