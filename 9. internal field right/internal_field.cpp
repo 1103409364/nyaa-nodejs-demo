c72275074c04b1ea0caae7fee7229207920deb23
wcs <wcs78929@gmail.com>
2022-12-03 13:38:18 +0800

    fix: 10. function template inherit
    Signed-off-by: wcs <wcs78929@gmail.com>

--- 9. internal field right/internal_field.cpp
+++ 9. internal field right/internal_field.cpp
@@ -2,74 +2,81 @@
 
 namespace __internal_field_right__ {
 
-using v8::FunctionCallbackInfo;
-using v8::HandleScope;
-using v8::ObjectTemplate;
-using v8::FunctionTemplate;
+using v8::Array;
+using v8::External;
 using v8::Function;
+using v8::FunctionCallbackInfo;
+using v8::FunctionTemplate;
+using v8::HandleScope;
 using v8::Isolate;
 using v8::Local;
-using v8::Persistent;
-using v8::External;
+using v8::MaybeLocal;
+using v8::Number;
 using v8::Object;
+using v8::ObjectTemplate;
+using v8::Persistent;
 using v8::String;
 using v8::Value;
-using v8::Number;
-using v8::Array;
-using v8::MaybeLocal;
 
 struct PersistentWrapper {
-    Persistent<Object> persistent;
-    int value;
+  Persistent<Object> persistent;
+  int value;
 };
 
-void WeakCallback(const v8::WeakCallbackInfo<PersistentWrapper>& data)
-{
-    PersistentWrapper* wrapper = data.GetParameter();
+void WeakCallback(const v8::WeakCallbackInfo<PersistentWrapper> &data) {
+  PersistentWrapper *wrapper = data.GetParameter();
+  // No member named 'IsNearDeath' in 'v8::Persistent<v8::Object>'
+  //   if (!wrapper->persistent.IsNearDeath()) {
+  //     printf("WARN...\n");
+  //   }
 
-    if(!wrapper->persistent.IsNearDeath())
-    {
-        printf("WARN...\n");
-    }
-
-    printf("deleting 0x%.8X: %d...", wrapper, wrapper->value); // NOLINT
-    wrapper->persistent.Reset();
-    delete wrapper;
-    printf("ok\n");
+  printf("deleting 0x%.8X: %d...", wrapper, wrapper->value); // NOLINT
+  wrapper->persistent.Reset();
+  delete wrapper;
+  printf("ok\n");
 }
 
-void CreateObject(const FunctionCallbackInfo<Value>& args)
-{
-    Isolate* isolate = args.GetIsolate();
+void CreateObject(const FunctionCallbackInfo<Value> &args) {
+  Isolate *isolate = args.GetIsolate();
 
-    // 新建对象模板
-    Local<ObjectTemplate> templ = ObjectTemplate::New(isolate);
-    templ->SetInternalFieldCount(1);
+  // 新建对象模板
+  Local<ObjectTemplate> templ = ObjectTemplate::New(isolate);
+  templ->SetInternalFieldCount(1);
 
-    // 新建对象以及设置内置字段
-    PersistentWrapper* wrapper = new PersistentWrapper();
-    wrapper->value = args[0]->ToNumber()->Int32Value();
-    Local<Object> obj = templ->NewInstance();
-    obj->SetInternalField(0, External::New(isolate, wrapper));
+  // 新建对象以及设置内置字段
+  PersistentWrapper *wrapper = new PersistentWrapper();
+  wrapper->value = args[0]
+                       ->ToNumber(v8::Context::New(isolate))
+                       .ToLocalChecked()
+                       ->Int32Value(v8::Context::New(isolate))
+                       .ToChecked();
+  Local<Object> obj =
+      templ->NewInstance(v8::Context::New(isolate)).ToLocalChecked();
+  obj->SetInternalField(0, External::New(isolate, wrapper));
 
-    // 基于 obj 新建持久句柄
-    wrapper->persistent.Reset(isolate, obj);
+  // 基于 obj 新建持久句柄
+  wrapper->persistent.Reset(isolate, obj);
 
-    // 将持久句柄设置为弱持久句柄
-    wrapper->persistent.SetWeak(wrapper, WeakCallback, v8::WeakCallbackType::kInternalFields);
-    wrapper->persistent.MarkIndependent();
+  // 将持久句柄设置为弱持久句柄
+  wrapper->persistent.SetWeak(wrapper, WeakCallback,
+                              v8::WeakCallbackType::kInternalFields);
+  //   No member named 'MarkIndependent' in 'v8::Persistent<v8::Object>'
+  //   wrapper->persistent.MarkIndependent();
 
-    args.GetReturnValue().Set(obj);
+  args.GetReturnValue().Set(obj);
 }
 
-void Init(Local<Object> exports, Local<Object> module)
-{
-    Isolate* isolate = Isolate::GetCurrent();
-    HandleScope scope(isolate);
+void Init(Local<Object> exports, Local<Object> module) {
+  Isolate *isolate = Isolate::GetCurrent();
+  HandleScope scope(isolate);
 
-    exports->Set(String::NewFromUtf8(isolate, "create"), FunctionTemplate::New(isolate, CreateObject)->GetFunction());
+  (void)exports->Set(v8::Context::New(isolate),
+                     String::NewFromUtf8(isolate, "create").ToLocalChecked(),
+                     FunctionTemplate::New(isolate, CreateObject)
+                         ->GetFunction(v8::Context::New(isolate))
+                         .ToLocalChecked());
 }
 
 NODE_MODULE(_template, Init)
 
-}
+} // namespace __internal_field_right__
